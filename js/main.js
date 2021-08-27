a6087def1c77703e8838e442ed4026dd5cd7a868
kunal2899 <kunaljain649@gmail.com>
2021-08-27 21:36:13 +0530

    instruction page added

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// function check(){
+//     let sub = document.getElementsByClassName('start')[0]
+//     if("disabled" in sub.classList){
+//         sub.classList.remove(['disabled'])
+//     }else{
+//         sub.classList.add(['disabled'])
+//     }
+// }
