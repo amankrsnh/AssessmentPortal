96c843bd4aa55af5395161358ebe2ffe7a79ef27
kunal2899 <kunaljain649@gmail.com>
2021-08-28 23:47:40 +0530

    timer added

--- js/main.js
+++ js/main.js
@@ -6,3 +6,21 @@
 //         sub.classList.add(['disabled'])
 //     }
 // }
+
+function timer(){
+	const startingMinutes = 15;
+	let time = startingMinutes * 60;
+
+	const countdownEl = document.getElementById('countdown');
+
+    setInterval(function updateCountdown(){
+		const minutes= Math.floor(time / 60);
+		let seconds = time % 60;
+	
+		seconds = seconds < 10 ? '0' + seconds : seconds;
+	
+		countdownEl.innerHTML = minutes+':'+ seconds;
+		time--;
+	}, 1000);
+
+}
